83ee9d40771f68f0a140a06f9e2f6bb7fd62140b
igorodriguesaraujo <igorodrigues_@hotmail.com>
2025-08-10 02:00:05 -0300

    add PORT in .env

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,6 @@ import 'dotenv/config';
 import { app } from './app';
 
 app.listen({
+  host: '0.0.0.0',
   port: Number(process.env.PORT) || 3333
 }).then(() => console.log('Server running http:localhost:' + process.env.PORT + '/api/v1'))
