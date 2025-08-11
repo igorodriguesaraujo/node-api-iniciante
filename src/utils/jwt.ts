ece489fbcc08a03e872379604eb2ccf98f14b753
igorodriguesaraujo <igorodrigues_@hotmail.com>
2025-08-11 20:03:21 -0300

    feat: add utils with resouce of the sign and verify token

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,14 +1,18 @@
 import 'dotenv/config'
-import { sign } from "jsonwebtoken"
+import { sign, SignOptions, verify } from "jsonwebtoken"
 
 export async function generateToken(payload: any) {
+  const options: SignOptions = {
+    expiresIn: process.env.JWT_EXPIRES_IN as SignOptions['expiresIn'], // 7 Dias
+  }
+
   return sign(
-    payload,
+    { payload },
     process.env.JWT_SECRET_KEY!,
+    options
   )
 }
 
 export function verifyToken(token: string) {
-  const payload = JSON.parse(token)
-  return payload
+  return verify(token, process.env.JWT_SECRET_KEY!)
 }
