5f9ef43567aedc37eeefc520991c18d514f2b807
igorodriguesaraujo <igorodrigues_@hotmail.com>
2025-08-11 16:05:10 -0300

    feat: add setup create user with zod and fastify-type-provider-zod

--- src/http/routes/create-user.ts
+++ src/http/routes/create-user.ts
@@ -1,7 +1,50 @@
-import { FastifyInstance } from "fastify"
+import z from "zod/v4"
+import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod"
 
-export const createUserRoute = async function (app: FastifyInstance) {
-  app.post('/user', (request, reply) => {
-    return reply.status(201)
+import { createUser } from "../../service/create-user"
+import { getUserByEmail } from "../../service/get-user-by-email"
+
+import { generateHash } from "../../utils/hash"
+
+export const createUserRoute: FastifyPluginCallbackZod = async function (app) {
+  app.post('/users', {
+    schema: {
+      body: z.object({
+        name: z.string().min(1, { message: 'Campo obrigatório' }),
+        email: z.email({ message: 'E-mail inválido' }),
+        password: z.string().min(8, { message: 'Senha deve ter no mínimo 8 caracteres' })
+      })
+    }
+  }, async (request, reply) => {
+    const { name, email, password } = request.body
+
+    const userExists = await getUserByEmail(email)
+
+    if (userExists[0]) {
+      return reply.status(400).send({
+        error: true,
+        message: 'Usuário já cadastrado'
+      })
+    }
+
+    const hashPassword = await generateHash(password)
+
+    if (!hashPassword) {
+      return reply.status(500).send({
+        error: true,
+        message: 'Falha ao criar usuário'
+      })
+    }
+
+    await createUser({
+      name,
+      email,
+      password: hashPassword
+    })
+
+    return reply.status(201).send({
+      error: false,
+      message: 'Usuário cadastrado com sucesso'
+    })
   })
 }
