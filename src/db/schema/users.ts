4fe6f7ada07adf397275f152736d7f4790cecd44
igorodriguesaraujo <igorodrigues_@hotmail.com>
2025-08-11 14:35:21 -0300

    feat: Add schema user on database

---
+++ src/db/schema/users.ts
@@ -0,0 +1,9 @@
+import { pgTable, timestamp, uuid, text } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: uuid().primaryKey().defaultRandom(),
+  name: text("name").notNull(),
+  email: text("email").notNull(),
+  password: text("password").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
