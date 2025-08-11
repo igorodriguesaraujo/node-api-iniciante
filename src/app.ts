dd95004067f1cffb732e3ab26283aca2613eef2d
igorodriguesaraujo <igorodrigues_@hotmail.com>
2025-08-11 16:03:52 -0300

    feat: Add config with fastify-type-provider-zod and zod

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,17 @@ import { createUserRoute } from "./http/routes/create-user";
 import { getUsersRoute } from "./http/routes/get-users";
 import { updateUsersRoute } from "./http/routes/update-user";
 import { removeUsersRoute } from "./http/routes/remove-user";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
 
 export const app = fastify()
 
 const prefix = { prefix: '/api/v1' };
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.withTypeProvider<ZodTypeProvider>()
 
 app.register(cors, {
   origin: '*'
