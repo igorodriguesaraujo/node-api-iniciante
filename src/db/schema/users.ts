de13852f85b4c89d7de05879a704b07949f1ad89
igorodriguesaraujo <igorodrigues_@hotmail.com>
2025-08-12 02:27:40 -0300

    feat: add field email_verified on schema users

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -1,9 +1,10 @@
-import { pgTable, timestamp, uuid, text } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, uuid, text, boolean } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: uuid().primaryKey().defaultRandom(),
   name: text("name").notNull(),
   email: text("email").unique().notNull(),
   password: text("password").notNull(),
+  email_verified: boolean("email_verified").default(false).notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
